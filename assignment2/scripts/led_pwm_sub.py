0c67db052747113bc69324cc7fe62e9837a19881
taishiyamamoto <ambitious0816@gmail.com>
2019-01-14 08:02:59 +0000

    fix

---
+++ assignment2/scripts/led_pwm_sub.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import rospy
+import roslib
+import subprocess
+from std_msgs.msg import UInt8
+import wiringpi as w
+import time
+
+PIN_LED=25
+OUTPUT=1
+
+def callback(blink):
+	w.softPwmWrite(PIN_LED,blink.data)
+	rospy.loginfo("LED_PWM_VALUE=%d",blink.data)
+
+def led_control():
+	subprocess.check_call('gpio export 25 out',shell=True)
+	rospy.init_node('led_pwm_sub')
+	w.wiringPiSetupSys()
+	w.pinMode(PIN_LED,OUTPUT)
+	w.softPwmCreate(PIN_LED,0,255)
+	rospy.Subscriber("blink",UInt8,callback)
+	rospy.spin()
+
+if __name__=='__main__':
+	led_control()
